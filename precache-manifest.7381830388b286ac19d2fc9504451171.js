723c505ff17c46974429e932f5c9676b130e8f9b
shubhamojha <shubham.ojha@bajajfinserv.in>
2021-08-27 20:14:44 +0530

    Updates

---
+++ precache-manifest.7381830388b286ac19d2fc9504451171.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0515f6822c741781b95cac63228a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2de34e213519299627",
+    "url": "/static/css/main.a747952d.chunk.css"
+  },
+  {
+    "revision": "77982cbc1bb1480310b2",
+    "url": "/static/js/2.f20b7e9d.chunk.js"
+  },
+  {
+    "revision": "24dba4e0a7b41a2da7c5559f3b2138ff",
+    "url": "/static/js/2.f20b7e9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef2de34e213519299627",
+    "url": "/static/js/main.bc07148a.chunk.js"
+  },
+  {
+    "revision": "931686523e4ddd616378",
+    "url": "/static/js/runtime-main.92e1f69d.js"
+  },
+  {
+    "revision": "2bb1c7ee6473d29ed269ed534c8e497a",
+    "url": "/static/media/1.2bb1c7ee.jpg"
+  },
+  {
+    "revision": "dcbef1d87727d7f83a52f71a13ea93e9",
+    "url": "/static/media/1.dcbef1d8.png"
+  },
+  {
+    "revision": "89de80d93cac15dad157d4afee3e0ef5",
+    "url": "/static/media/2.89de80d9.png"
+  },
+  {
+    "revision": "e5d8b206c5a883cd68c99041b4a3b5ae",
+    "url": "/static/media/2.e5d8b206.jpg"
+  },
+  {
+    "revision": "572c57633d124dc89e7c9e3e8a5321ea",
+    "url": "/static/media/HighlandGothicLightFLF.572c5763.ttf"
+  },
+  {
+    "revision": "ebb22cfb2a2ea3cca5cedfaebb6bef6e",
+    "url": "/static/media/MarketsHome.ebb22cfb.png"
+  },
+  {
+    "revision": "3f9219880ea302d431e93d4a644c665c",
+    "url": "/static/media/MarketseStore.3f921988.png"
+  },
+  {
+    "revision": "af642988dc2793c7879fcac31032b9fb",
+    "url": "/static/media/advanced_sans_serif_7.af642988.ttf"
+  },
+  {
+    "revision": "89197718912f23f118ebdb84eaf65067",
+    "url": "/static/media/dp2.89197718.jpeg"
+  },
+  {
+    "revision": "5128e51ecf792f9d39d3adc19f3e162b",
+    "url": "/static/media/logo.5128e51e.png"
+  },
+  {
+    "revision": "d71873af137f1f76ecc4ac02da1570e6",
+    "url": "/static/media/logo.d71873af.png"
+  }
+]);
